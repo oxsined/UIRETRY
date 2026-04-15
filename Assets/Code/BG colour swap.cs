58272fae418fc597a1dcce9152bfe9949fe278c7
TPODanzig <tpodanzig@gmail.com>
2026-04-15 21:17:13 +0200

    Colour fade

--- Assets/Code/BG colour swap.cs
+++ Assets/Code/BG colour swap.cs
@@ -5,7 +5,12 @@ using UnityEngine.UI;
 
 public class BGcolourswap : MonoBehaviour
 {
+    [SerializeField] float colourSwapSpeed;
+
     private RawImage backgroundImage;
+
+    private Color currentColour;
+    private Color targetColour;
 
     [SerializeField] Button[] buttons;
     //Buttons: 0 = 2D Work   1 = 3D Work   2 = Envoirments   3 = Projects
@@ -14,20 +19,29 @@ public class BGcolourswap : MonoBehaviour
     {
         //selecteer de achtergrond
         backgroundImage = GetComponent<RawImage>();
+        currentColour = Color.white; // initialize to your default background colour
     }
 
     void Update()
     {
-        backgroundImage.color = Color.white;
-        //eerst zet je de kleur op wit (makkelijker dan checken of je op niks hoverd)
+        backgroundImage.color = currentColour;
+
+        targetColour = Color.white;
+        //zet de kleur op wit (makkelijker dan checken of je op niks hoverd)
 
         foreach (var button in buttons) //check alle buttons
         {
             if (button.GetComponent<ButtonHover>().isHovered)
             {
-                backgroundImage.color = button.GetComponent<ButtonHover>().BackgroundColour;
+                targetColour = button.GetComponent<ButtonHover>().BackgroundColour;
                 //als je op een button hoverd zet het de achtergrond kleur op de kleur die staat op die button in het ButtonHover script
             }
+        }
+
+        if (targetColour != currentColour)
+        {
+            currentColour = Color.Lerp(currentColour, targetColour, colourSwapSpeed * Time.deltaTime);
+            //smooth fade naar de target kleur
         }
     }
 }
